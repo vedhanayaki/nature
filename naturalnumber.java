b40daa231fb2cb0ffe34b77f53b5507b872a29a9
vedhanayaki <vedhanayaki1996@gmail.com>
2017-06-29 12:04:50 +0530

    Add files via upload

---
+++ naturalnumber.java
@@ -0,0 +1,14 @@
+package guviset3;
+
+public class naturalnumber {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the values");
+		int tem=0;
+		for(int i=0;i<30;i++){
+			tem=tem+i;
+		}
+		System.out.println("values"+tem);
+	}
+
+}
